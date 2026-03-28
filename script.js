beebbbe77bd1d7ff23df9a0ba7114fb20b7942d5
AnthonyBaek <120500222+AnthonyBaek@users.noreply.github.com>
2026-03-28 22:06:15 +0900

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('themeToggle').addEventListener('click', () => {
-  const current = document.documentElement.getAttribute('data-theme');
-  document.documentElement.setAttribute('data-theme', current === 'dark' ? 'light' : 'dark');
-});
